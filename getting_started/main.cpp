6a6e017ef1ff1b5a064edefdeae5d6c5d522d5b0
Branislav Popadic <popadicbranislav@gmail.com>
2021-01-23 00:17:40 +0100

    Getting started - camera - look at

--- getting_started/main.cpp
+++ getting_started/main.cpp
@@ -20,16 +20,10 @@ void handleInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 1024;
 const unsigned int SCR_HEIGHT = 768;
 
-float xOffset = 0.0f;
-float yOffset = 0.0f;
-
-float zoomLevel = -3.0f;
-
-float FOV = 45.0f;
-
 int main(int, char **)
 {
 	// glfw initialize and configure
+	// -----------------------------
 	glfwInit();
 	setupWindowHints();
 
@@ -53,13 +47,17 @@ int main(int, char **)
 		return -2;
 	}
 
+	// configure global opengl state
+	// -----------------------------
+	glEnable(GL_DEPTH_TEST);
+
 	// build and compile our shader program
+	// ------------------------------------
 	Shader ourShader("/home/brano/Code/Projects/learnopengl/getting_started/shaders/shader.vertex",
 					 "/home/brano/Code/Projects/learnopengl/getting_started/shaders/shader.fragment");
 
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
-
 	float cube_vertices[] = {
 		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
 		0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
@@ -103,23 +101,27 @@ int main(int, char **)
 		-0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
 		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f};
 
-	// unsigned int indices[] = {
-	// 	0, 1, 3, // first triangle
-	// 	1, 2, 3	 // second triangle
-	// };
+	// world space positions of our cubes
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f, 0.0f, 0.0f),
+		glm::vec3(2.0f, 5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f, 3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f, 2.0f, -2.5f),
+		glm::vec3(1.5f, 0.2f, -1.5f),
+		glm::vec3(-1.3f, 1.0f, -1.5f)};
 
 	unsigned int VBO, VAO; //, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	// glGenBuffers(1, &EBO);
-	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
-
-	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	// glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// position attribute
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
@@ -145,8 +147,7 @@ int main(int, char **)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	// load image, create texture and generate mipmaps
 	int width, height, nrChannels;
-	stbi_set_flip_vertically_on_load(true);
-
+	stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
 	unsigned char *data = stbi_load("/home/brano/Code/Projects/learnopengl/getting_started/resources/textures/container.jpg", &width, &height, &nrChannels, 0);
 	if (data)
 	{
@@ -163,7 +164,7 @@ int main(int, char **)
 	glGenTextures(1, &texture2);
 	glBindTexture(GL_TEXTURE_2D, texture2);
 	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	// set texture filtering parameters
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -172,6 +173,7 @@ int main(int, char **)
 	data = stbi_load("/home/brano/Code/Projects/learnopengl/getting_started/resources/textures/awesomeface.png", &width, &height, &nrChannels, 0);
 	if (data)
 	{
+		// note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}
@@ -181,23 +183,27 @@ int main(int, char **)
 	}
 	stbi_image_free(data);
 
+	// tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
+	// -------------------------------------------------------------------------------------------
 	ourShader.use();
 	ourShader.setInt("texture1", 0);
 	ourShader.setInt("texture2", 1);
 
-	glEnable(GL_DEPTH_TEST);
+	// set up camera
+	// glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
 
-	glm::vec3 cubePositions[] = {
-		glm::vec3(0.0f, 0.0f, 0.0f),
-		glm::vec3(2.0f, 5.0f, -15.0f),
-		glm::vec3(-1.5f, -2.2f, -2.5f),
-		glm::vec3(-3.8f, -2.0f, -12.3f),
-		glm::vec3(2.4f, -0.4f, -3.5f),
-		glm::vec3(-1.7f, 3.0f, -7.5f),
-		glm::vec3(1.3f, -2.0f, -2.5f),
-		glm::vec3(1.5f, 2.0f, -2.5f),
-		glm::vec3(1.5f, 0.2f, -1.5f),
-		glm::vec3(-1.3f, 1.0f, -1.5f)};
+	// glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+	// glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+
+	// glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+	// glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+
+	// glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
+	// glm::mat4 view;
+	// view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f),
+	// 					glm::vec3(0.0f, 0.0f, 0.0f),
+	// 					glm::vec3(0.0f, 1.0f, 3.0f));
 
 	// render loop
 	// -----------
@@ -221,24 +227,27 @@ int main(int, char **)
 		//activate shader
 		ourShader.use();
 
-		// // create transformations
-		glm::mat4 view = glm::mat4(1.0f);
+		// calculate camera position
+		float radius = 10.0f;
+		float zCameraPos = cos((float)glfwGetTime()) * radius;
+		float xCameraPos = sin((float)glfwGetTime()) * radius;
+		glm::mat4 view;
+		view = glm::lookAt(glm::vec3(xCameraPos, 0.0f, zCameraPos),
+							glm::vec3(0.0f, 0.0f, 0.0f),
+							glm::vec3(0.0f, 1.0f, 0.0f));
+
+		// create transformations
+		// glm::mat4 view = glm::mat4(1.0f); // make sure to initialize matrices to identity matrix first
 		glm::mat4 projection = glm::mat4(1.0f);
+		// view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		// pass transformation matrices to the shader
+		ourShader.setMat4("view", view);
+		ourShader.setMat4("projection", projection);
 
-		view = glm::translate(view, glm::vec3(xOffset, yOffset, zoomLevel));
-		projection = glm::perspective(glm::radians(FOV), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
-		unsigned int modelLocation = glGetUniformLocation(ourShader.ID, "model");
-		// glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
-
-		unsigned int viewLocation = glGetUniformLocation(ourShader.ID, "view");
-		glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
-
-		unsigned int projectionLocation = glGetUniformLocation(ourShader.ID, "projection");
-		glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, glm::value_ptr(projection));
-
-		// render container
-		glBindVertexArray(VAO);
+			// render boxes
+			glBindVertexArray(VAO);
 		for (unsigned int i = 0; i < 10; i++)
 		{
 			// calculate the model matrix for each object and pass it to shader before drawing
@@ -246,7 +255,7 @@ int main(int, char **)
 			model = glm::translate(model, cubePositions[i]);
 			float angle = i % 3 != 0 ? 20.0f * i : (float)glfwGetTime() * 25.0f; // every third cube including 1st one rotates over time
 			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-			glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+			ourShader.setMat4("model", model);
 
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
@@ -260,7 +269,6 @@ int main(int, char **)
 	// ------------------------------------------------------------------------
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	// glDeleteBuffers(1, &EBO);
 
 	// glfw: terminate, clearing all previously allocated GLFW resources.
 	// ------------------------------------------------------------------
@@ -294,39 +302,15 @@ void handleInput(GLFWwindow *window)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
 
-	// if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-	// {
-	// 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	// }
+	// Set wireframe view mode on 'W' key press
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	}
 
+	// Set fill view mode on 'F' key press
 	if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
 	{
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-	}
-
-	float step = 0.1f;
-	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-	{
-		yOffset -= step;
-	}
-	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-	{
-		yOffset += step;
-	}
-	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-	{
-		xOffset += step;
-	}
-	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-	{
-		xOffset -= step;
-	}
-	if (glfwGetKey(window, GLFW_KEY_KP_ADD) == GLFW_PRESS)
-	{
-		zoomLevel -= step;
-	}
-	if (glfwGetKey(window, GLFW_KEY_KP_SUBTRACT) == GLFW_PRESS)
-	{
-		zoomLevel += step;
 	}
 }
