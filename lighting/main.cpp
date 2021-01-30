8a6760cdda2e055a3a48df63e5813a9f48d66c87
Branislav Popadic <popadicbranislav@gmail.com>
2021-01-30 23:39:09 +0100

    Lighting - light casters - done

--- lighting/main.cpp
+++ lighting/main.cpp
@@ -216,6 +216,7 @@ int main(int, char **)
 		lightingShader.setVec3("light.position", camera.Position);
 		lightingShader.setVec3("light.direction", camera.Front);
 		lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+		lightingShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
 		lightingShader.setVec3("viewPos", camera.Position);
 
 		lightingShader.setVec3("light.ambient", 0.1f, 0.1f, 0.1f);
