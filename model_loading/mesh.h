d906aa3aadcc113c852683541eb0732f32908635
Branislav Popadic <popadicbranislav@gmail.com>
2021-02-04 21:39:51 +0100

    Model loading - Mesh

--- model_loading/mesh.h
+++ model_loading/mesh.h
@@ -41,12 +41,39 @@ public:
 
     setupMesh();
   }
-  void Draw(Shader &shader);
+
+  // render the mesh
+  void Draw(Shader &shader)
+  {
+    unsigned int diffuseNr = 1;
+    unsigned int specularNr = 1;
+    for (unsigned int i = 0; i < textures.size(); i++)
+    {
+      glActiveTexture(GL_TEXTURE0 + i); // activate proper texture unit before binding
+      // retrieve texture number (the N in diffuse_textureN)
+      string number;
+      string name = textures[i].type;
+      if (name == "texture_diffuse")
+        number = std::to_string(diffuseNr++);
+      else if (name == "texture_specular")
+        number = std::to_string(specularNr++);
+
+      shader.setFloat(("material." + name + number).c_str(), i);
+      glBindTexture(GL_TEXTURE_2D, textures[i].id);
+    }
+    glActiveTexture(GL_TEXTURE0);
+
+    // draw mesh
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+  };
 
 private:
   // render data
   unsigned int VAO, VBO, EBO;
 
+  // initializes all the buffer objects
   void setupMesh()
   {
     glGenVertexArrays(1, &VAO);
@@ -76,7 +103,6 @@ private:
                           (void *)offsetof(Vertex, TexCoords));
 
     glBindVertexArray(0);
-    
   };
 };
 
