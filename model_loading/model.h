250c7c58558f3072b2ef21321f24bbb6507af69a
Branislav Popadic <popadicbranislav@gmail.com>
2021-02-04 22:51:37 +0100

    Model loading -  Model pt.2 - optimizations

--- model_loading/model.h
+++ model_loading/model.h
@@ -39,6 +39,7 @@ public:
 
 private:
     // model data
+    vector<Texture> textures_loaded;
     vector<Mesh> meshes;
     string directory;
 
@@ -140,11 +141,25 @@ private:
         {
             aiString str;
             mat->GetTexture(type, i, &str);
-            Texture texture;
-            texture.id = TextureFromFile(str.C_Str(), directory);
-            texture.type = typeName;
-            texture.path = str.C_Str();
-            textures.push_back(texture);
+            bool skip = false;
+            for (unsigned int j = 0; j < textures_loaded.size(); j++)
+            {
+                if (std::strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
+                {
+                    textures.push_back(textures_loaded[j]);
+                    skip = true;
+                    break;
+                }
+            }
+            if (!skip)
+            { // if texture hasn't been loaded already, load it
+                Texture texture;
+                texture.id = TextureFromFile(str.C_Str(), directory);
+                texture.type = typeName;
+                texture.path = str.C_Str();
+                textures.push_back(texture);
+                textures_loaded.push_back(texture); // add to loaded textures
+            }
         }
         return textures;
     };
